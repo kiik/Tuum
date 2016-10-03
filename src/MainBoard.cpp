090f9ec8839bbe9db1543cc2a1bf3777e9e2b5d5
Meelik Kiik <kiik.meelik@gmail.com>
2016-10-03 20:42:44 +0300

    Initial commit.

---
+++ src/MainBoard.cpp
@@ -0,0 +1,131 @@
+
+#include <sstream>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "MainBoard.hpp"
+
+namespace tuum { namespace hal {
+
+  const char CMD_BALL_SENSE[] = "bl";
+  const char CMD_DRIBBLER[] = "dm";
+  const char CMD_COIL[] = "c";
+  const char CMD_KICK[] = "k";
+  const char CMD_WKICK[] = "k3";
+
+  MainBoard::MainBoard() {
+    id = 255;
+
+    m_ballSensorState = 0;
+    m_dribblerState = 0;
+    m_coilKickActive = 0;
+
+    m_coilKickCharge.setPeriod(300);
+    m_coilKickCooldown.setPeriod(1500);
+
+    m_updateTimer.setPeriod(300);
+    m_updateTimer.start();
+
+    senseBall();
+  }
+
+  void MainBoard::init(RTX485::WriteHandle wHandle, RTX485::SignalService sigRegister) {
+    RTX485::DeviceCallback cb = std::bind1st(std::mem_fun(&MainBoard::signal), this);
+    RTX485::Device::init(wHandle, sigRegister, cb);
+  }
+
+  void MainBoard::signal(RTX485::Message m) {
+    uint32_t ix = m.data.find(":");
+    if(ix != -1) {
+      std::string buf;
+      buf = m.data.substr(0, ix);
+
+      if(buf == CMD_BALL_SENSE) {
+        m_ballSensorState = atoi(m.data.substr(ix+1).c_str());
+      }
+    }
+  }
+
+  void MainBoard::run() {
+    if(m_updateTimer.isTime()) {
+      if(m_dribblerState) {
+        startDribbler();
+      }
+      m_updateTimer.start();
+    }
+
+    if(m_coilKickActive && m_coilKickCharge.isTime()) {
+      if(m_coilChargeLevel > 4) {
+        m_coilKickActive = false;
+	m_coilKickCooldown.start();
+      } else {
+      	chargeCoil();
+        m_coilChargeLevel++;
+	if(m_coilChargeLevel == 1) releaseCoil();
+	else chargeCoil();
+	m_coilKickCharge.start();
+      }
+
+    }
+  }
+
+  bool MainBoard::getBallSensorState() {
+    return m_ballSensorState;
+  }
+
+  void MainBoard::senseBall() {
+    send({id, CMD_BALL_SENSE});
+  }
+
+
+  void MainBoard::chargeCoil() {
+    send({id, CMD_COIL});
+  }
+
+  void MainBoard::releaseCoil() {
+    if(m_coilKickStrong)
+      send({id, CMD_KICK});
+    else
+      send({id, CMD_WKICK});
+  }
+
+  void MainBoard::coilKick() {
+    if(!m_coilKickActive && m_coilKickCooldown.isTime()) {
+      senseBall();
+      chargeCoil();
+      m_coilKickActive = true;
+      m_coilChargeLevel = 0;
+      m_coilKickCharge.start();
+    }
+  }
+
+  void MainBoard::doCoilKick() {
+    m_coilKickStrong = true;
+    coilKick();
+  }
+
+  void MainBoard::doWeakCoilKick() {
+    m_coilKickStrong = false;
+    coilKick();
+  }
+
+  std::string getDribblerCmd(int v) {
+    std::stringstream out;
+    out << CMD_DRIBBLER;
+    out << v;
+    return out.str();
+  }
+
+  void MainBoard::startDribbler() {
+    m_dribblerState = 1;
+    send({id, getDribblerCmd(140)});
+  }
+
+  void MainBoard::stopDribbler() {
+    m_dribblerState = 0;
+    send({id, getDribblerCmd(0)});
+  }
+
+
+
+}}
---
+++ src/MotorControl.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <cmath>
+
+#include "MotorControl.hpp"
+
+namespace tuum { namespace hal {
+
+  const char CMD_MOTOR_SPEED[] = "sd";
+
+  MotorControl::MotorControl():
+    RTX485::Device::Device()
+  {
+
+  }
+
+  void MotorControl::init(RTX485::WriteHandle wHandle) {
+    int id_seq, ix;
+    for(ix=0, id_seq=1; ix < MOTOR_COUNT; ix++, id_seq++) {
+      m_motorIDs[ix] = id_seq;
+    }
+
+    RTX485::Device::init(wHandle);
+
+    std::cout << "[MotorControl::init]Ready." << std::endl;
+  }
+
+
+  std::string MotorControl::getSpeedCmd(int v) {
+    std::stringstream out;
+    out << CMD_MOTOR_SPEED;
+    out << v;
+    return out.str();
+  }
+
+  void MotorControl::OmniDrive(double speed, double angle, double rot) {
+    int spd1 = speed * sin(angle + M_PI / 4.0) + rot;
+    int spd2 = speed * -sin(angle - M_PI / 4.0) + rot;
+    int spd3 = speed * -sin(angle + M_PI / 4.0) + rot;
+    int spd4 = speed * sin(angle - M_PI / 4.0) + rot;
+
+    int speeds[4] = {spd1, spd2, spd3, spd4};
+    for (int ix=0; ix < MOTOR_COUNT; ix++) {
+      send({m_motorIDs[ix], getSpeedCmd(speeds[ix])});
+      usleep(10);
+    }
+  }
+
+  void MotorControl::stop() {
+    for (int ix=0; ix < MOTOR_COUNT; ix++) {
+      send({m_motorIDs[ix], getSpeedCmd(0)});
+      usleep(10);
+    }
+  }
+
+}}
