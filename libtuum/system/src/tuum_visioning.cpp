3a0db643a0978b524860fa9791d601c174429d5c
Meelik Kiik <kiik.meelik@gmail.com>
2016-10-31 20:57:40 +0200

    Implement discrete YUV color space conversion.

--- libtuum/system/src/tuum_visioning.cpp
+++ libtuum/system/src/tuum_visioning.cpp
@@ -9,6 +9,7 @@
 #include "mathematicalConstants.hpp"
 
 #include "tuum_logger.hpp"
+#include "tuum_fs.hpp"
 #include "tuum_ogl.hpp"
 #include "tuum_ogl_core.hpp"
 
@@ -18,6 +19,7 @@
 #include "tuum_localization.hpp"
 
 using namespace tuum::hal;
+using namespace std;
 
 namespace tuum {
 
@@ -29,7 +31,15 @@ namespace tuum {
   const char* DEF_PPL_BLOBS  = "./assets/blobs.ppl";
   const char* DEF_PPL_GEOM = "./assets/geometry.ppl";
 
+  const char* DEF_DISC_YUV = "./assets/dYUV.dat";
+
   vision::LayoutLoader* gLdr = create_layout_loader();
+
+  File* discYUVfp = NewFile(DEF_DISC_YUV);
+  image_t discrete_yuv;
+
+  TxFormat* txDiscYUVFormat = nullptr;
+  Texture*  txDiscYUV = nullptr;
 
   Visioning::Visioning():
     m_cam_N(1), m_lid(0)
@@ -52,15 +62,37 @@ namespace tuum {
     m_format[0] = new TxFormat(iprop.width, iprop.height, GL_RGB, GL_UNSIGNED_BYTE);
     m_tex = new Texture(*m_format[0]);
 
+    txDiscYUVFormat = new TxFormat(32*32, 16, GL_RGB, GL_UNSIGNED_BYTE);
+
     gLdr->addRequiredElement("inputFormat", *m_format[0]);
 
     m_plSimplify.use(gLdr);
-    m_plImFormat.use(gLdr);
+    //m_plImFormat.use(gLdr);
     //m_plGeom.use(gLdr);
 
-    m_plSimplify.load(DEF_PPL_NONE, "SimplifyPipe");
-    m_plImFormat.load(DEF_PPL_NONE, "ImFormatPipe");
+    m_plSimplify.load(DEF_PPL_SIMPL, "SimplifyPipe");
+    //m_plImFormat.load(DEF_PPL_NONE, "ImFormatPipe");
     //m_plGeom.load(DEF_PPL_GEOM);
+
+    char* data;
+
+    Env::spawnBuffer(discrete_yuv);
+    char* d;
+    size_t size;
+
+    if(discYUVfp->bread(d, size) >= 0) {
+      discrete_yuv->init(size);
+      memcpy((uint8_t*)discrete_yuv->data, (uint8_t*)d, size);
+
+      printf("%d, %d, %d; len=%lu\n", d[3], d[4], d[5], discrete_yuv->size);
+
+      txDiscYUV = new Texture(*txDiscYUVFormat);
+      txDiscYUV->write(discrete_yuv->data, GL_RGB, GL_UNSIGNED_BYTE);
+
+    } else {
+      RTXLOG(format("Discrete YUV space load failed ( '%s' )", DEF_DISC_YUV), LOG_ERR);
+    }
+
 
     RTXLOG("Ready.");
   }
@@ -104,6 +136,8 @@ namespace tuum {
     return 0;
   }
 
+
+  bool dbg = false;
   int Visioning::doFramePass()
   {
     Pipeline *ppl, *nppl;
@@ -112,15 +146,28 @@ namespace tuum {
       size_t id;
       while(!m_iFrameLock.try_lock()) {};
       m_tex->write(m_iFrame->data, GL_RGB, GL_UNSIGNED_BYTE);
+      txDiscYUV->write(discrete_yuv->data, GL_RGB, GL_UNSIGNED_BYTE);
       id = m_iFrame->id;
       m_iFrameLock.unlock();
 
       ppl = m_plSimplify.get();
-      (*ppl) << (*m_tex) << Pipeline::Process;
 
+      uint8_t* d;
+      if(!dbg) {
+        //d = (uint8_t*)discrete_yuv->data;
+        //printf("pre: %d, %d, %d\n", d[0], d[1], d[2]);
+
+        d = (uint8_t*)discrete_yuv->data;
+        printf("pre: %d, %d, %d\n", d[0], d[1], d[2]);
+      }
+
+      (*ppl) << (*m_tex) << (*txDiscYUV) << Pipeline::Process;
+
+      /*
       nppl = m_plImFormat.get();
       (*nppl) << ppl->out(0) << Pipeline::Process;
       ppl = nppl;
+      */
 
       //nppl = m_plGeom.get();
       //(*nppl) << ppl->out(0) << ppl->out(1) << Pipeline::Process;
@@ -130,6 +177,12 @@ namespace tuum {
       ppl->out(0).read(m_oFrame->data);
       m_oFrame->id = id;
       m_oFrameLock.unlock();
+
+      if(!dbg) {
+        d = (uint8_t*)m_oFrame->data;
+        printf("res: %d, %d, %d\n", d[0], d[1], d[2]);
+        dbg = true;
+      }
 
       return 1;
     } catch(Glip::Exception& e) {
